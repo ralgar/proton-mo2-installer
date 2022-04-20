9575fb143517f71f6c4d2ce49a0da667f094a19f
Ryan Algar <59636191+ralgar@users.noreply.github.com>
2022-04-20 12:56:21 -0700

    Complete rewrite of the NXM broker script

--- handlers/nxm-broker.sh
+++ handlers/nxm-broker.sh
@@ -1,45 +1,126 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
+# nxm-broker.sh - Launcher script for the Mod Organizer 2 NXM Handler.
+#
+# This script is called by a MIME type handler, and simply launches
+#   the NXM handler inside the prefix of a running MO2 instance.
+# If MO2 isn't running, then we start it and pass the download as a launch argument.
+#
+# Copyright: (c) 2022, Ryan Algar (https://github.com/ralgar/proton-mo2-installer)
+#
+# GNU General Public License v3.0 (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-###    PARSE POSITIONAL ARGS    ###
-nxm_link=$1; shift
 
-if [ -z "$nxm_link" ]; then
-	echo "ERROR: please specify a NXM Link to download"
-	exit 1
-fi
+# Creates various GUI dialog boxes
+_dialog() {
 
-nexus_game_id=${nxm_link#nxm://}
-nexus_game_id=${nexus_game_id%%/*}
-###    PARSE POSITIONAL ARGS    ###
+	local type
+	local msg
 
-instance_link="$HOME/.local/share/steamplay-skyrim-tools/modorganizer2/${nexus_game_id:?}"
-instance_dir=$(readlink -f  "$instance_link")
-if [ ! -d "$instance_dir" ]; then
-	[ -L "$instance_link"] && rm "$instance_link"
+    case $1 in
+        error)
+            type=error
+            case $2 in
+                invalid_url)
+                    msg="\nAn invalid URL was provided. Aborting download."
+                    ;;
+                no_url)
+                    msg="\nA URL was not provided. Cannot start download."
+                    ;;
+                missing_mo2)
+                    msg="\nCannot find Mod Organizer 2 instance for game.\n\n$nexus_game_id"
+                    ;;
+                *)
+                    return 1 ;;
+            esac
+            ;;
+        *)
+            return 1 ;;
+    esac
 
-	zenity --ok-label=Exit --ellipsize --error --text \
-		"Could not download file because there is no Mod Organizer 2 instance for '$nexus_game_id'"
-	exit 1
-fi
+    zenity \
+        --title="NXM Handler" \
+        --$type --icon-name "dialog-$type" \
+        --no-wrap --ellipsize \
+        --text "$msg"
+}
 
-instance_dir_windowspath="Z:$(sed 's/\//\\\\/g' <<<$instance_dir)"
-pgrep -f "$instance_dir_windowspath\\\\ModOrganizer.exe"
-process_search_status=$?
 
-game_appid=$(cat "$instance_dir/appid.txt")
+# Check if an MO2 instance exists for Nexus Game ID
+_get_mo2_instance_status() {
 
-if [ "$process_search_status" == "0" ]; then
-	echo "INFO: sending download '$nxm_link' to running Mod Organizer 2 instance"
-	download_start_output=$(WINEESYNC=1 WINEFSYNC=1 protontricks-launch --appid "$game_appid" "$instance_dir/nxmhandler.exe" "$nxm_link")
-	download_start_status=$?
+	if [ -d "$instance_dir" ]; then
+		game_appid=$(cat "$instance_dir/appid.txt")
+	else
+        _dialog error missing_mo2
+		return 1
+	fi
+}
+
+
+# Main function
+_main() {
+
+	local game_appid
+	local nxm_url="$1"
+	local data_dir="${XDG_DATA_HOME:-$HOME/.local/share}"
+
+	# Check and parse the NXM URL
+	_parse_url "$nxm_url" || return 1
+
+	# Make sure an MO2 instance exists for the game
+	_get_mo2_instance_status || return 1
+
+	# Start the download, and MO2 if necessary
+	_start_download || return 1
+}
+
+
+# Checks and parses an NXM URL
+_parse_url() {
+
+	if [[ -z $* ]]; then
+		echo "ERROR: Please specify an NXM URL to download."
+        _dialog error no_url
+		return 1
+	elif [[ $# -gt 1 ]] ; then
+		echo "ERROR: Too many arguments were passed."
+        _dialog error invalid_url
+		return 1
+	elif [[ ! $nxm_url =~ nxm:// ]] ; then
+		echo "ERROR: The NXM URL is malformed or otherwise invalid."
+        _dialog error invalid_url
+		return 1
+	fi
+
+	nexus_game_id=${nxm_url#nxm://}
+	nexus_game_id=${nexus_game_id%%/*}
+
+	instance_dir="$data_dir/proton-mo2-installer/modorganizer2/${nexus_game_id:?}"
+}
+
+
+_start_download() {
+
+	# If MO2 is running, then pass the URL to it, else start MO2
+	if pgrep -f "ModOrganizer.exe" ; then
+		printf "INFO: Sending download to running Mod Organizer 2 instance.\n"
+		if ! WINEESYNC=1 WINEFSYNC=1 protontricks-launch \
+				--appid "$game_appid" "$instance_dir/nxmhandler.exe" "$nxm_url"
+		then
+			return 1
+		fi
+	else
+		printf "INFO: MO2 is not running. launching it now.\n"
+		if ! steam -applaunch "$game_appid" "$nxm_url" ; then
+			return 1
+		fi
+	fi
+}
+
+
+if _main "$@" ; then
+	exit 0
 else
-	echo "INFO: starting Mod Organizer 2 to download '$nxm_link'"
-	download_start_output=$(steam -applaunch "$game_appid" "$nxm_link")
-	download_start_status=$?
-fi
-
-if [ "$download_start_status" != "0" ]; then
-	zenity --ok-label=Exit --ellipsize --error --text \
-		"Failed to start download:\n\n$download_start_output"
 	exit 1
 fi
