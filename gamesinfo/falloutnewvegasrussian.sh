2c75a0089b66638b1abcadb2f20ff1a907f30f21
Ryan Algar <59636191+ralgar@users.noreply.github.com>
2022-08-05 00:06:16 -0700

    Fix #6: Bump NVSE version to 6.2.8

--- gamesinfo/falloutnewvegasrussian.sh
+++ gamesinfo/falloutnewvegasrussian.sh
@@ -6,5 +6,5 @@ game_nexusid="newvegas"
 game_appid=22490
 game_executable="FalloutNVLauncher.exe"
 game_protontricks=("d3dcompiler_43" "d3dx9")
-game_scriptextender_url="https://github.com/xNVSE/NVSE/releases/download/6.2.4/nvse_6_2_4.7z"
+game_scriptextender_url="https://github.com/xNVSE/NVSE/releases/download/6.2.8/nvse_6_2_8.7z"
 game_scriptextender_strip_dirs=0
