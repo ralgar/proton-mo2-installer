c4038be765182ab19b235c288dc44f589aa24fa8
Ryan Algar <59636191+ralgar@users.noreply.github.com>
2022-08-04 23:47:47 -0700

    Fix #5: Update F4SE URL and strip leading dir
    - Config: Bump F4SE URL to 0.6.23.
    - Config: Strip leading directory when extracting archive.

--- gamesinfo/fallout4.sh
+++ gamesinfo/fallout4.sh
@@ -6,5 +6,5 @@ game_nexusid="fallout4"
 game_appid=377160
 game_executable="Fallout4Launcher.exe"
 game_protontricks=("xaudio2_7=native")
-game_scriptextender_url="https://f4se.silverlock.org/beta/f4se_0_06_21.7z"
-game_scriptextender_strip_dirs=0
+game_scriptextender_url="https://f4se.silverlock.org/beta/f4se_0_06_23.7z"
+game_scriptextender_strip_dirs=1
