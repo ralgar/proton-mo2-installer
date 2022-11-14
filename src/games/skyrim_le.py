095604982728c9d05ed6488b914da51976ca3ea7
Ryan Algar <59636191+ralgar@users.noreply.github.com>
2022-11-13 20:30:17 -0800

    Clean up game classes

--- src/games/skyrim_le.py
+++ src/games/skyrim_le.py
@@ -6,15 +6,23 @@ class SkyrimLE:
 
     def __init__(self):
 
-        self.app_id = 72850
-        self.name   = "Skyrim LE"
+        self.app_id       = 72850
+        self.executable   = "SkyrimLauncher.exe"
+        self.name         = "Skyrim LE"
+        self.nexus_id     = "skyrim"
+        self.protontricks = [ "d3dcompiler_43", "d3dx9" ]
+        self.subdirectory = "Skyrim"
 
 
     def __str__(self):
 
-        self_dict = {
-            "app_id": self.app_id,
-            "name": self.name
+        describe = {
+            "app_id":       self.app_id,
+            "executable":   self.executable,
+            "name":         self.name,
+            "nexus_id":     self.nexus_id,
+            "protontricks": self.protontricks,
+            "subdirectory": self.subdirectory,
         }
 
-        return str(self_dict)
+        return str(describe)
--- src/games/skyrim_se.py
+++ src/games/skyrim_se.py
@@ -6,15 +6,23 @@ class SkyrimSE:
 
     def __init__(self):
 
-        self.app_id = 489830
-        self.name   = "Skyrim SE"
+        self.app_id       = 489830
+        self.executable    = "SkyrimSELauncher.exe"
+        self.name         = "Skyrim SE"
+        self.nexus_id     = "skyrimspecialedition"
+        self.protontricks = [ "xaudio2_7=native" ]
+        self.subdirectory = "Skyrim Special Edition"
 
 
     def __str__(self):
 
-        self_dict = {
-            "app_id": self.app_id,
-            "name": self.name
+        describe = {
+            "app_id":       self.app_id,
+            "executable":   self.executable,
+            "name":         self.name,
+            "nexus_id":     self.nexus_id,
+            "protontricks": self.protontricks,
+            "subdirectory": self.subdirectory,
         }
 
-        return str(self_dict)
+        return str(describe)
