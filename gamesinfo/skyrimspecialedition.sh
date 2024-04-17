986d5cb375cb81979d493247de431ad4e1eab44e
Ryan Algar <me@ralgar.dev>
2024-04-17 13:07:17 -0700

    fix: update SKSE version for Skyrim SE/AE

--- gamesinfo/skyrimspecialedition.sh
+++ gamesinfo/skyrimspecialedition.sh
@@ -6,5 +6,5 @@ game_nexusid="skyrimspecialedition"
 game_appid=489830
 game_executable="SkyrimSELauncher.exe"
 game_protontricks=("xaudio2_7=native")
-game_scriptextender_url="https://github.com/ianpatt/skse64/releases/download/v2.2.2/skse64_2_02_02.7z"
+game_scriptextender_url="https://github.com/ianpatt/skse64/releases/download/v2.2.6/skse64_2_02_06.7z"
 game_scriptextender_strip_dirs=1
